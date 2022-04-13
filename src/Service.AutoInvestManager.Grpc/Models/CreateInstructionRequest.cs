53348ee2bbecb61328383309c0ad5d584334b58c
Pavlo Pylypenko <pavel.p@smplt.net>
2022-04-13 10:24:07 +0300

    fix

--- src/Service.AutoInvestManager.Grpc/Models/CreateInstructionRequest.cs
+++ src/Service.AutoInvestManager.Grpc/Models/CreateInstructionRequest.cs
@@ -11,10 +11,6 @@ namespace Service.AutoInvestManager.Grpc.Models
         [DataMember(Order = 2)] public string BrokerId { get; set; }
         [DataMember(Order = 3)] public string WalletId { get; set; }
         [DataMember(Order = 7)] public ScheduleType ScheduleType { get; set; }
-        [DataMember(Order = 8)] public DateTime ScheduledTime { get; set; }
-        [DataMember(Order = 9)] public DayOfWeek ScheduledDayOfWeek { get; set; }
-        [DataMember(Order = 10)] public int ScheduledDayOfMonth { get; set; }
-        
         [DataMember(Order = 12)] public string QuoteId { get; set; }
 
 
